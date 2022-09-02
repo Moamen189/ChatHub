1df1cd705d87d7f4f0b4b11b1091b323f5274b34
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-09-02 22:10:21 +0200

    EFDataBaseFirst

--- ChatHub/Hubs/ChatHub.cs
+++ ChatHub/Hubs/ChatHub.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using ChatHub.Contexts;
+using ChatHub.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace ChatHub.Hubs
@@ -7,7 +10,12 @@ namespace ChatHub.Hubs
     {
         public async Task Send(string user, string message)
         {
-           await Clients.All.SendAsync("ReceiveMessage",user, message); //Fire Event
+           await Clients.All.SendAsync("ReceiveMessage",user, message ); //Fire Event
+
+            ChatContext Context = new ChatContext();
+            Message messageObj = new Message() { message_text = message, user_name = user, message_date = message  };
+            Context.Messages.Add(messageObj);
+            Context.SaveChanges();
         }
     }
 }
