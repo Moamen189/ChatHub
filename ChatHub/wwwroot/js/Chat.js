88f7aa293a79470658119cd59fa1806d6ab32edc
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2022-09-02 20:55:04 +0200

    UpdateChat

--- ChatHub/wwwroot/js/Chat.js
+++ ChatHub/wwwroot/js/Chat.js
@@ -1,1 +1,9 @@
-﻿ 
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var messageInp = document.getElementById("messageInp");
+    var userName = prompt("Please Enter Your Name : ");
+    var proxeyConnection = new signalR.HubConnectionBuider().withUrl("https://localhost:44334/chat").build(); //Create Proxy
+    //document.getElementById("sendBtn").addEventListener("click", function () {
+
+    //});
+
+});
